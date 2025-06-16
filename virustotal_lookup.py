6062b3d087cbe369ba1a8de81ec81240ad94c836
kpoh0003 <kpoh0003@student.monash.edu>
2025-06-16 13:05:23 +1000

    adding function for user input (hash/url/ip)

--- virustotal_lookup.py
+++ virustotal_lookup.py
@@ -5,3 +5,8 @@ import base64
 # Function to get API Key from user
 def get_api_key():
     return input("Please enter your VirusTotal API Key: ").strip()
+
+# Function to get User Input
+def user_input():
+    return input("🧪 Please enter a hash, URL, or IP address to check: ").strip()
+
